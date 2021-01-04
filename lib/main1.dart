242b7ab6fcd007c5c494327848209c188b0a14e7
pacopro3 <paco.juarez.3@gmail.com>
2021-01-04 14:36:59 -0600

    Se realiza la integración de firebase para pruebas

---
+++ lib/main1.dart
@@ -0,0 +1,29 @@
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', ''),
+      ],
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: getAplicationRoutes(),
+      onGenerateRoute: (settings) =>
+          MaterialPageRoute(builder: (BuildContext context) => AlertPage()),
+    );
+  }
+}
